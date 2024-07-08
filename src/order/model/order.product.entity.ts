7ffcab6fefc2d9b92f4e6c11f527a07a34c65357
Ahmed Abbas <ahmed.mohamed.sayed.11@gmail.com>
2024-07-08 13:51:47 +0300

    Add arabic name & desc in order product.

--- src/order/model/order.product.entity.ts
+++ src/order/model/order.product.entity.ts
@@ -14,7 +14,15 @@ export class OrderProduct {
 
   @IsNotEmpty()
   @Column()
+  name_ar: string;
+
+  @IsNotEmpty()
+  @Column()
   description: string;
+
+  @IsNotEmpty()
+  @Column()
+  description_ar: string;
 
   @IsNotEmpty()
   @Column()
